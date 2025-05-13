f9336f4370acb3ca17d4dcf8ca7ed468cc0b34a4
Lucas Bressan <lucasbbressan22@gmail.com>
2025-05-13 13:37:40 -0300

    Add files via upload

---
+++ todolist/script.js
@@ -0,0 +1,21 @@
+function addTask() {
+  const taskInput = document.getElementById("taskInput");
+  const taskList = document.getElementById("taskList");
+  const taskText = taskInput.value.trim();
+
+  if (taskText !== "") {
+    const li = document.createElement("li");
+    li.textContent = taskText;
+
+    const removeBtn = document.createElement("button");
+    removeBtn.textContent = "Remover";
+    removeBtn.className = "remove-btn";
+    removeBtn.onclick = function () {
+      taskList.removeChild(li);
+    };
+
+    li.appendChild(removeBtn);
+    taskList.appendChild(li);
+    taskInput.value = "";
+  }
+}
